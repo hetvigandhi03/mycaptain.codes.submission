b65ef8550146d63d9041fa3762e62e9d64ea3694
hetvigandhi03 <75487912+hetvigandhi03@users.noreply.github.com>
2020-12-04 19:55:40 +0530

    did the first program of finding the size
    this file prints size of int char float and double

---
+++ program1_findsize.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of char: "<<sizeof(char);
+    cout<<" byte\n";
+    cout<<"size of int: "<<sizeof(int);
+    cout<<" bytes\n";
+    cout<<"size of float: "<<sizeof(float);
+    cout<<" bytes\n";
+    cout<<"size of double: "<<sizeof(double);
+    cout<<" bytes\n";
+    return 0;
+}
